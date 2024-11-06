79d46850d4670aeaa7d5e5eda37f6dfce40f7f46
mabol4194 <mabol4194@usn.no>
2024-11-06 14:15:41 +0100

    Update Arbeidskrav_1_Samanlikning_av_årlige_kostnadar_for_bensinbil_og_elbil.py

--- Arbeidskrav_1_Samanlikning_av_årlige_kostnadar_for_bensinbil_og_elbil.py
+++ Arbeidskrav_1_Samanlikning_av_årlige_kostnadar_for_bensinbil_og_elbil.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 """
 Arbeidskrav 1
 Samanlikning av årlige konstadar for elbil og bensinbil.
@@ -11,10 +5,6 @@ Samanlikning av årlige konstadar for elbil og bensinbil.
 Skrevet av Mari Nesse Bolstad
 2024 11 06
 """
-
-
-# In[67]:
-
 
 # Årlige kostnadar for bensinbil
 
@@ -25,10 +15,6 @@ BB = 0.3*10000 # bomavgift
 
 B  = FB + AB + DB + BB
 
-
-# In[68]:
-
-
 # Årlige kostnadar for elbil
 
 FE = 5000 # forsikring
@@ -37,10 +23,6 @@ DE = (0.2*10000)*2.0 # drivstoffbruk
 BB = 0.1*10000 # bomavgift
 
 E = FE + AE + DE + BB
-
-
-# In[69]:
-
 
 print("Bensinbil =", B , "kr")
 print("Elbil =" , E , "kr")
